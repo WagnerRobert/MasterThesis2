03a68b3c8767fdf7f10c7b9b36a258996b84ede9
WagnerRobert <robertbei@gmx.de>
2014-01-28 01:32:43 +0100

    update

--- masterthesis/processProtein.py
+++ masterthesis/processProtein.py
@@ -1,5 +1,20 @@
 __author__ = 'delur'
+import sys
 
 def processProtein(name, kmerlists, result_info, tree):
+    pro_kmerlist = []
+    con_kmerlist = []
+    if result_info[0] in tree[0]:
+        print tree[0]
+        pro_kmerlist = kmerlists[1]
+        con_kmerlist = kmerlists[0]
+    elif result_info[0] in tree[1]:
+        print tree[1]
+        pro_kmerlist = kmerlists[0]
+        con_kmerlist = kmerlists[1]
+
+    clean_name = name.split('#')[0]
+    getUniprot(clean_name)
+    sys.exit()
 
     pass

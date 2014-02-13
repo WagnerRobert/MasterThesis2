8b62add89b1ca9ea9aa9ea8c2b8172afcfb80c29
wagnerr <robertbei@gmx.de>
2014-02-13 17:36:49 +0100

    working on numKmers

--- queue_main.py
+++ queue_main.py
@@ -445,7 +445,7 @@ def doZPlot():
             import matplotlib.pyplot as plt
             x = [1] * len (zscores)
             x.extend([2] * len (zscores))
-            zscores.extend(zscores)
+            zscores.concatenate(zscores)
 
 
             plt.plot(x,zscores, 'o')

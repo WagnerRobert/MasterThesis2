5a87daf9cbed53fe2e5f5141b946f119ce9c2a3e
wagnerr <robertbei@gmx.de>
2014-02-13 17:54:50 +0100

    working on numKmers

--- queue_main.py
+++ queue_main.py
@@ -452,7 +452,7 @@ def doZPlot():
             for protein in svmLocList[svm][location]:
                 index += 1
                 values = []
-                for kmer,value in kmerlist[svm][protein][0]:
+                for kmer,value in svmLocList[svm][protein][0]:
                     values.append(value)
                 zscores_protein = stats.zscore(values)
 

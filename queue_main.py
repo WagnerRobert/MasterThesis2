891f63ef11626a5357e87e2253194547d4a80f8c
WagnerRobert <robertbei@gmx.de>
2014-02-17 09:51:01 +0100

    :

--- queue_main.py
+++ queue_main.py
@@ -505,12 +505,13 @@ def doZPlot():
             ax1.xaxis.grid(True)
             # ax1.margins(0.04)
 
-            # ax2.bar(range(len(kmernumbers)), kmernumbers)
-            # ax2.set_xticks(range(len(kmernumbers)))
-            # ax2.set_xticklabels(kmernames, rotation=30)
+
             plt.savefig(os.path.join(constants["pdf"], "zscore_" + location+".pdf"))
 
-
+            plt.bar(range(len(kmernumbers)), kmernumbers)
+            plt.set_xticks(range(len(kmernumbers)))
+            plt.set_xticklabels(kmernames, rotation=30)
+            plt.savefig(os.path.join(constants["pdf"], "zscore_outliers" + location+".pdf"))
             #sys.exit()
 
 

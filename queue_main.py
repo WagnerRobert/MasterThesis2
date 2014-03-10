7ae7649efcb6c91cf700812467a33910d4ffcaf8
wagnerr <robertbei@gmx.de>
2014-03-10 18:08:33 +0100

    working on numKmers

--- queue_main.py
+++ queue_main.py
@@ -31,6 +31,7 @@ quant = 0.01
 # sets up the directory in which all calculations will be done
 setUp(constants)
 result = reader.read_resultfile(constants)
+print result
 tree = reader.read_treefile(constants)
 reader.read_kmerfiles(constants, quant) #reads and prepares kmerweights
 #files, saves complete result (dict[svm][protein]) kmers.pkl in pickles dir

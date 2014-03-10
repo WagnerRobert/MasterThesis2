87ac444822f53a336a8e1fbfcda44bda5a9328a3
wagnerr <robertbei@gmx.de>
2014-03-10 16:45:07 +0100

    working on numKmers

--- topKmers.py
+++ topKmers.py
@@ -130,36 +130,31 @@ neg = 1
 
 #cytopla_dict = kmer_dir("/mnt/project/locbloc-ha/studs/robert/euka_small/cytopla", constants)
 #write_picklefile(cytopla_dict,"cytopla_dict", constants)
-print "reading cytopla pickle file"
-cytopla_dict = read_picklefile("cytopla_dict", constants)
+#print "reading cytopla pickle file"
+#cytopla_dict = read_picklefile("cytopla_dict", constants)
 
-print "starting cyto zscore stuff now"
-cytopla_dict = zscore(cytopla_dict, neg)
-write_picklefile(cytopla_dict,"cytopla_dict_z", constants)
+#print "starting cyto zscore stuff now"
+#cytopla_dict = zscore(cytopla_dict, neg)
+#write_picklefile(cytopla_dict,"cytopla_dict_z", constants)
+cytopla_dict = read_picklefile("cytopla_dict_z", constants)
 
 #nucleus_dict = kmer_dir("/mnt/project/locbloc-ha/studs/robert/euka_small/nucleus", constants)
 #write_picklefile(nucleus_dict,"nucleus_dict", constants)
-print "reading nucleus pickle file"
-nucleus_dict = read_picklefile("nucleus_dict", constants)
+#print "reading nucleus pickle file"
+#nucleus_dict = read_picklefile("nucleus_dict", constants)
 
-print "starting nucleus zscore stuff now"
-nucleus_dict = zscore(nucleus_dict, pos)
-write_picklefile(nucleus_dict,"nucleus_dict_z", constants)
-
-
-
+#print "starting nucleus zscore stuff now"
+#nucleus_dict = zscore(nucleus_dict, pos)
+#write_picklefile(nucleus_dict,"nucleus_dict_z", constants)
+nucleus_dict = read_picklefile("nucleus_dict_z", constants)
 
 
 
 
 
-print "starting zscore stuff now"
-pos = 0
-neg = 1
-cytopla_dict = zscore(cytopla_dict, neg)
-write_picklefile(cytopla_dict,"cytopla_dict_z", constants)
-nucleus_dict = zscore(nucleus_dict, pos)
-write_picklefile(nucleus_dict,"nucleus_dict_z", constants)
+
+
+
 
 removelist = []
 for index1, element1 in enumerate(sorted(cytopla_dict.iteritems(), key=operator.itemgetter(1), reverse=True)):

1e21d455bf8f0c33b6acb931472abd694085e98d
wagnerr <robertbei@gmx.de>
2014-03-25 19:24:18 +0100

    starting on correctPredictedCleanKmers

--- correctPredictedCleanKmers.py
+++ correctPredictedCleanKmers.py
@@ -59,7 +59,7 @@ locTree2Uniprot["nucleus"] = "nucleus"
 locTree2Uniprot["cellmemb"] = "plasma_membrane"
 locTree2Uniprot["memmitoc"] = "mitochondria_membrane"
 locTree2Uniprot["peroxis"] = "peroxisome"
-locTree2Uniprot["memmitoc"] = "mitochondria_membrane"
+locTree2Uniprot["mitochon"] = "mitochondria"
 
 #cross check loc2Prot against locSeqDict, to find Proteines that are wrongfully predicted to be in a location
 for localisation in loc2prot:

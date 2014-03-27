0cea9e752635a8f192ad87db3aa8361420b21b98
wagnerr <robertbei@gmx.de>
2014-03-27 15:56:56 +0100

    starting on correctPredictedCleanKmers

--- countKmerHits.py
+++ countKmerHits.py
@@ -42,7 +42,7 @@ for location in locKmerDict:
     locKmerDictList[location] = sorted(locKmerDictList[location], key=operator.itemgetter(1), reverse=True)
     locKmerDictList[location] = [i[0] for i in locKmerDictList[location]]
 print "reading all the annotated sequences from swissprot"
-f = open("/mnt/project/locbloc-ha/sp042011/SP13_11/eukaryotes.SP13_11.expSL.50.Before05_11.new.fa", 'r')
+f = open("/mnt/project/locbloc-ha/studs/robert/euka_small/eukaryota.1682.fa", 'r')
 name = ""
 localisation = ""
 locSeqDict= {}

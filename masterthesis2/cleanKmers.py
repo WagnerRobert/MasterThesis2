78161d5871e6639071db41e4372c20b455fecfc9
WagnerRobert <robertbei@gmx.de>
2014-04-07 22:53:51 +0200

    messing with blastProtein

--- masterthesis2/cleanKmers.py
+++ masterthesis2/cleanKmers.py
@@ -58,7 +58,7 @@ def cleanKmers(locKmerList, locSeqDict):
                     for kmer in locKmerDict[location2][protein]:
                         print kmer
                         loc2KmerSet.add(kmer)
-                localRemovelist = [val for val in loc1KmerList if val in loc2KmerSet]
+                localRemovelist = [val for val in locKmerDict if val in loc2KmerSet]
                 #for kmer in loc1Keys:
                 #    if kmer in loc2Keys:
                 #        removeList.append(kmer)

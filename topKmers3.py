b781cef62a12be679ed360fb7a1e6ec66bf167ab
WagnerRobert <robertbei@gmx.de>
2014-04-10 11:14:31 +0200

    messing with blastProtein

--- topKmers3.py
+++ topKmers3.py
@@ -132,11 +132,11 @@ for location in counts:
     i = 0
     keepGoing = True
     workedList = []
-    for index, subsetA in enumerate(sorted(counts[location], key= lambda x : len(x), reverse=True)):
+    for index, subsetA in enumerate(sorted(counts[location], key= lambda x : counts[location][x], reverse=True)):
         print str(index) + "/" + str(len(counts[location])) + "\t" + str(subsetA)
         sys.stdout.flush()
         for subsetB in sorted(counts[location], key= lambda x : len(x)):
-            if counts[location][subsetA] < counts[location][subsetB]:
+            if counts[location][subsetA] > counts[location][subsetB]:
                 if set(subsetA).issuperset(subsetB):
                     print str(subsetA) + " is superset of " + str(subsetB)
                     sys.stdout.flush()

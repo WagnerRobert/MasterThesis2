2cd1a23875cd7d22f084d0f55922f26955aacba6
wagnerr <robertbei@gmx.de>
2014-04-11 14:57:26 +0200

    starting on correctPredictedCleanKmers

--- topKmers3.py
+++ topKmers3.py
@@ -99,7 +99,7 @@ for line in f:
 f.close()
 
 print "\nChecking top kmers against NLSdb motifs:"
-for kmer in locZscore["nucleus"]:
+for kmer in locKmerList["nucleus"]:
     print kmer
     for motif in motifs:
         if len(kmer) <= len(motif):

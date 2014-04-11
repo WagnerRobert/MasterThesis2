ab84f785f506e6d3996ffdc8d52cb06fbd227d07
wagnerr <robertbei@gmx.de>
2014-04-11 17:00:45 +0200

    starting on correctPredictedCleanKmers

--- topKmers3.py
+++ topKmers3.py
@@ -107,7 +107,7 @@ for protein in locSeqDict["nucleus"]:
     for motif in motifs:
         match = re.findall(motif, locSeqDict["nucleus"][protein])
         if match:
-            print protein + "\t" + str(match)
+            print protein + "\t" + str(match) + "\t" + motif
 
 f = open("NLS_clear_experimental_partial.txt", 'r')
 partial_motifs = []

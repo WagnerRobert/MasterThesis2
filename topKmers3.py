12d59cca4538b63575eaa4a5dd20c9c849999c6a
wagnerr <robertbei@gmx.de>
2014-04-14 12:55:24 +0200

    starting on correctPredictedCleanKmers

--- topKmers3.py
+++ topKmers3.py
@@ -75,7 +75,7 @@ for location in locKmerDict:
     locKmerList = sorted(locKmerList, key=operator.itemgetter(1), reverse=True)
     for kmer, value in locKmerList:
             if kmer not in topKmer_dict[location]:
-                if len(topKmer_dict[location]) < 100000 or True:
+                if len(topKmer_dict[location]) < 10000 or False:
                     topKmer_dict[location][kmer] = []
                 else:
                     break
